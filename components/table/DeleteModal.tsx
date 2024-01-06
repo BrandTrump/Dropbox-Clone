b465906060831834f0ef3b788e4c8e879d901f89
Brandon Trump <brandontrump101@gmail.com>
2024-01-06 23:11:08 +1300

    feat: added delete functionality

---
+++ components/table/DeleteModal.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { db, storage } from "@/firebase";
+import { useAppStore } from "@/store/store";
+import { useUser } from "@clerk/nextjs";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+
+export function DeleteModal() {
+  const { setIsDeleteModalOpen, isDeleteModalOpen, fileId } = useAppStore();
+
+  const { user } = useUser();
+
+  async function deletFile() {
+    if (!user || !fileId) return;
+
+    const fileRef = ref(storage, `users/${user.id}/files/${fileId}`);
+
+    try {
+      deleteObject(fileRef);
+      deleteDoc(doc(db, "users", user.id, "files", fileId))
+        .then(() => {
+          // toast notification
+        })
+        .finally(() => {
+          setIsDeleteModalOpen(false);
+        });
+    } catch (error) {
+      // toast notification
+    }
+  }
+  return (
+    <Dialog
+      open={isDeleteModalOpen}
+      onOpenChange={(isOpen) => {
+        setIsDeleteModalOpen(isOpen);
+      }}
+    >
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Are you sure you want to delete?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            file!
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex space-x-2 py-3">
+          <Button
+            size={"sm"}
+            className="px-3 flex-1"
+            variant={"secondary"}
+            onClick={() => setIsDeleteModalOpen(false)}
+          >
+            <span className="sr-only">Cancel</span>
+            <span>Cancel</span>
+          </Button>
+
+          <Button
+            type="submit"
+            size="sm"
+            className="px-3 flex-1"
+            onClick={() => deletFile()}
+          >
+            <span className="sr-only">Delete</span>
+            <span>Delete</span>
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
